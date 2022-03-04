22f3b1682abcf3691a9fce1cd6c9ffb04bd57bb6
navabhi22 <navabhi22@gmail.com>
2022-03-05 02:00:46 +0530

    Greeting message PP02 Stack and queue

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PP02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PP02 Stack and Queue");
+        }
+    }
+}
