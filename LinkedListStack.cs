6d757a50ec16e7c1d38c62324e858d6d35b0b8da
navabhi22 <navabhi22@gmail.com>
2022-03-05 02:16:20 +0530

    Abhinav Added Peek and Pop

--- LinkedListStack.cs
+++ LinkedListStack.cs
@@ -37,6 +37,33 @@ namespace PP02
             }
             
         }
+        internal void Peek()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            Console.WriteLine("{0} is top of the stack ", this.top.data);
+        }
+        internal void Pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty, Deletion is not possible");
+                return ;
+            }
+            Console.WriteLine("value popped is {0} ", this.top.data);
+            this.top = this.top.next;
+        }
+        internal void IsEmpty()
+        {
+            while (this.top != null)
+            {
+                Peek();
+                Pop();
+            }
+        }
 
     }
 }
